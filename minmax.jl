92ed2fe033aa6bda68cfecf6179f1a86b22507b3
Nick Collins <nick@vividx.co.uk>
2013-04-08 18:11:26 -0400

    min or max option passed as token

--- minmax.jl
+++ minmax.jl
@@ -24,33 +24,34 @@ function possible_moves(board)
     out
 end
 
-function minmax_with_cache(board, player, func)
+
+function minmax_with_cache(board, player, status)
+    func = (status == :max) ? indmax : indmin
     # this needs to return the utility (for recursion)
     # and also the best move
     if has(cache, player) && has(cache[player], board)
         cache[player][board]
     else
         child_player = -1 * player
-        child_func = (func == indmax) ? indmin : indmax
+        child_status = (status == :max) ? (:min) : (:max)
+        child_func = (child_status == :max) ? indmax : indmin
         if game_state(board) == :continue_game
             children = []
             for next_move=possible_moves(board)
                 child_board = move(board, player, next_move)
-                util, best_move = minmax_with_cache(child_board, child_player, child_func)
+                util, _ = minmax_with_cache(child_board, child_player, child_status)
                 children = vcat(children, [(util, next_move)])
             end
             # this adds the best (util, best_move) pair to the cache
-            util, best_move = children[child_func([c[1] for c=children])]
-            if func == indmax
-                cache[player][board] = util, best_move
+            util, chosen_move = children[child_func([c[1] for c=children])]
+            if status == :max
+                cache[player][board] = util, chosen_move
             end
-            util, best_move
+            util, chosen_move
         else
             cache[player][board] = game_state(board), nothing
         end
     end
 end
 
-minmax_with_cache(board, player) = minmax_with_cache(board, player, indmax)
-
-
+minmax_with_cache(board, player) = minmax_with_cache(board, player, :max)
