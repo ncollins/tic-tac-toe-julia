971d9bb4fe8a878e92aa4e3beecb0726953d31f3
Nick Collins <nick@vividx.co.uk>
2013-04-09 14:33:32 -0400

    Corrected `minmax` and simplified caching.
    Using both `player` and `status` to decide on MIN or MAX was causing
    problems with the algorithm. Player `1` should always be maximizing and
    player `-1` should always be minimizing. This simplifies the caching,
    and in addition the board state determines whose turn it is removing the
    need for multiple dictionaries. The boards are now stored as strings to
    avoid any potential mutibility problems.

--- minmax.jl
+++ minmax.jl
@@ -1,17 +1,9 @@
 # Minmax.jl - minmax AI algorithm
 
 cache = Dict()
-cache[(-1,:max)] = Dict()
-cache[(-1,:min)] = Dict()
-cache[(1,:max)] = Dict()
-cache[(1,:min)] = Dict()
 
 function wipe_cache()
     cache = Dict()
-    cache[(-1,:max)] = Dict()
-    cache[(-1,:min)] = Dict()
-    cache[(1,:max)] = Dict()
-    cache[(1,:min)] = Dict()
 end
     
 
@@ -20,7 +12,6 @@ function possible_moves(board)
     for i=1:3
         for j=1:3
             if board[i,j] == 0
-                #@show j
                 out = vcat(out,[(i,j)])
             end
         end
@@ -29,32 +20,26 @@ function possible_moves(board)
 end
 
 
-function minmax(board, player, status)
-    func = (status == :max) ? indmax : indmin
+function minmax(board, player)
+    func = (player == 1) ? indmax : indmin
     # this needs to return the utility (for recursion)
     # and also the best move
-    if has(cache, (player, status)) && has(cache[(player, status)], board)
-        cache[(player, status)][board]
+    if has(cache, string(board))
+        cache[string(board)]
     else
         child_player = -1 * player
-        child_status = (status == :max) ? (:min) : (:max)
-        child_func = (child_status == :max) ? indmax : indmin
         if game_state(board) == :continue_game
             children = []
             for next_move=possible_moves(board)
                 child_board = move(board, player, next_move)
-                util, _ = minmax(child_board, child_player, child_status)
+                util, _ = minmax(child_board, child_player)
                 children = vcat(children, [(util, next_move)])
             end
-            # this adds the best (util, best_move) pair to the cache
-            #util, chosen_move = children[child_func([c[1] for c=children])]
             util, chosen_move = children[func([c[1] for c=children])]
-            cache[(player, status)][board] = util, chosen_move
+            cache[string(board)] = util, chosen_move
             util, chosen_move
         else
-            cache[(player, status)][board] = game_state(board), nothing
+            cache[string(board)] = game_state(board), nothing
         end
     end
 end
-
-minmax(board, player) = minmax(board, player, :max)
